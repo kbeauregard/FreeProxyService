4641d8c391a36a264bc9ab9d50fcd511ccadf34c
kbeauregard <kylembeauregard@gmail.com>
2018-08-19 09:55:37 -0400

    Added requirments

--- extraction.py
+++ extraction.py
@@ -1,6 +1,7 @@
 import requests
 from lxml import html
 
+
 def extract_freeproxy_page():
     headers = {
         'User-Agent': ('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36')
@@ -14,13 +15,13 @@ def extract_freeproxy_page():
         if len(data) != 8:
             continue
         proxies += [{
-            'ip': data[0].text_content(),
-            'port': data[1].text_content(),
-            'code': data[2].text_content(),
-            'country': data[3].text_content(),
-            'anonymity': data[4].text_content(),
-            'google': data[5].text_content(),
-            'https': data[6].text_content(),
+            'ip': data[0].text_content().lower(),
+            'port': data[1].text_content().lower(),
+            'code': data[2].text_content().lower(),
+            'country': data[3].text_content().lower(),
+            'anonymity': data[4].text_content().lower(),
+            'google': data[5].text_content().lower(),
+            'https': data[6].text_content().lower(),
             'last_checked': '',
         }]
     return proxies
